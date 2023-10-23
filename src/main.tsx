e53b3c812e256582f1d2a38b3cde3bc6bb0efcf6
axel <axel.cateland@dataiku.com>
2023-10-23 14:43:19 +0200

    session-6 react router integration part 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
-import Donations from './components/Donations.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Light from "./components/Light.tsx";
+import Sandbox from "./components/Sandbox.tsx";
+import Donations from "./components/Donations.tsx";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Donations />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { path: "/light", element: <Light /> },
+            { path: "/sandbox", element: <Sandbox /> },
+            { path: "/donations", element: <Donations /> },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
